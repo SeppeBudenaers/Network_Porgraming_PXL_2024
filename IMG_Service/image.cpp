36d9b2f44c731cf0c44cb59b521321a0f1743961
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2024-05-28 22:17:20 +0200

    Update image.cpp

--- IMG_Service/image.cpp
+++ IMG_Service/image.cpp
@@ -1,15 +1,11 @@
 #include "image.h"
 #include <iostream>
-#include <filesystem>
 #include "stb_image_write.h"
 
 void Image::SaveImage(QString SaveName)
 {
     QString outputPath = "./";
-    outputPath.append(User_Name);
-    outputPath.append("/");
     outputPath.append(SaveName);
-
     QByteArray decodedData = QByteArray::fromBase64(Image_Data.toUtf8());
     stbi_write_jpg(outputPath.toStdString().c_str(), Width, Height, Channels, decodedData.constData(), Width * Channels);
     std::cout << "Image saved to: " << outputPath.toStdString() << std::endl;
